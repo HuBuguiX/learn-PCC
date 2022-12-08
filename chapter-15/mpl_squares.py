79fac9f48ab681f3247af6d4e5b3e0ab3071f2fd
Bugui <hzh.banxi@qq.com>
2022-12-08 15:50:37 +0800

    添加对 plt.plot() 的注释

--- chapter-15/mpl_squares.py
+++ chapter-15/mpl_squares.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+# plt.plot() 根据参数绘制图形
 plt.plot(input_values, squares, linewidth=5)
 
 # 设置图表标题，并给坐标轴加上标签
